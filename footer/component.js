6c9eaaad64c0bfe5d833232525c97786560cf798
sdmaxim <sdmaxim@users.noreply.github.com>
2016-07-06 17:11:32 +0300

    06/07

--- footer/component.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.
-  module('footer').
-  component('footer', {
-    templateUrl: 'footer/template.html',
-    controller: function ContentViewController() {}
-  });
---
+++ push.bat
@@ -0,0 +1,10 @@
+git add angular 
+git add data 
+git add middle 
+git add nav-menu 
+git add top
+git add main.css
+git add module.js
+git add index.html
+git commit -m "%1"
+git push gulliver master
