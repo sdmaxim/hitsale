15d9ffc55ae3df439a3267f4d954ffef0ec93137
sdmaxim <sdmaxim@users.noreply.github.com>
2016-07-11 17:25:01 +0300

    11/07_17-24

--- slideshow/slideshow-c.js
+++ slideshow/slideshow-c.js
@@ -6,10 +6,31 @@ angular.
     templateUrl: 'slideshow/slideshow.html',
     controller: ['$routeParams', 'GetData', function ($routeParams, GetData) {
         var self = this;
+        self.ind = {
+          thumbLength : 7,
+          length : 0,
+          start : 0,
+          end : 0
+        }
+
+        self.getFirstImg = function () {
+          return self.files[0].fileId;
+        }
+
+        self.setInd = function () {
+          self.ind.length = self.data.length;
+          self.ind.end = self.data.length;
+          if (self.ind.end > self.ind.thumbLength) self.ind.end = self.ind.thumbLength;
+        }
+
         self.db = GetData.get({filename: $routeParams.pageId}, function(images) {
-        	self.files = images.data;
+          self.data = images.data;
+          self.setInd();
+
+        	self.thumbs = images.data.slice(self.ind.start, self.ind.end);
+
         	self.path = images.path + images.filename;
-          self.setImage(self.files[0].fileId);
+          self.setImage(self.getFirstImg);
         });
 
         self.setImage = function setImage(imageUrl) {
@@ -17,13 +38,39 @@ angular.
         };
 
         self.nextImage = function () {
-          self.temp = self.files.shift();
+          self.ind.start++;
+          self.ind.end++;
+
+          self.temp = self.thumbs.shift();
           self.files.push(self.temp);
+          return self.temp;
         }
 
         self.prevImage = function () {
           self.temp = self.files.pop();
           self.files.unshift(self.temp);
+        }
+
+        self.closePopUp = function(){
+          self.showPopUpMsg = false;
+        }
+
+        self.showPopUpMsg = false;
+
+        self.openPopUp = function( fileId ) {
+          var x = fileId * 1;
+          /*while(x > 0) {
+            self.nextImage();
+            console.log(fileId + " " + x);
+            x--;
+          }*/
+          self.setImage(fileId);
+          self.showPopUpMsg = true;
+        }
+
+        self.nextBigImage = function () {
+          self.nextImage();
+          self.setImage(self.files[0].fileId);
         }
 
       }
