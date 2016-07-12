5b9b16051da8eee5da5414783bd6c15cf54f0a0e
sdmaxim <sdmaxim@users.noreply.github.com>
2016-07-12 16:42:45 +0300

    12/07_16-42

--- slideshow/slideshow-c.js
+++ slideshow/slideshow-c.js
@@ -6,50 +6,86 @@ angular.
     templateUrl: 'slideshow/slideshow.html',
     controller: ['$routeParams', 'GetData', function ($routeParams, GetData) {
         var self = this;
+        self.fullPath = "";
         self.ind = {
           thumbLength : 7,
           length : 0,
           start : 0,
-          end : 0
+          end : 0,
+          bigInd : 0,
+          nextImage : function () {
+            this.bigInd++;
+            this.start++;
+            this.end++;
+            if (this.start > this.length-1) {
+              this.start = 0;
+            }
+            if (this.end > this.length-1) {
+              this.end = 0;
+            }
+            if (this.bigInd > this.length-1) {
+              this.bigInd = 0;
+            }
+
+            self.thumbs.shift();
+            self.thumbs.push(self.data[this.end]);
+
+            console.log(this.start + " " + this.end);
+          },
+          prevImage : function () {
+            this.start--;
+            this.end--;
+            this.bigInd--;
+            if (this.start < 0) {
+              this.start = this.length-1;
+            }
+            if (this.end < 0) {
+              this.end = this.length-1;
+            }
+            if (this.bigInd < 0) {
+              this.bigInd = this.length-1;
+            }
+
+            self.thumbs.pop();
+            self.thumbs.unshift(self.data[this.start]);
+            console.log(this.start + " " + this.end);
+          }
         }
 
-        self.getFirstImg = function () {
-          return self.files[0].fileId;
+        self.getFileId = function (fileId) {
+          if (!fileId) fileId = 0;
+          fileId += self.ind.start;
+          if (fileId > self.ind.length-1)
+          fileId -= self.ind.length - 1;
+          return fileId;
         }
 
-        self.setInd = function () {
+        self.getFullPathSmall = function (fileId) {
+          fileId = self.getFileId(fileId);
+          return self.fullPath + self.data[fileId].fileId + "-small.jpg";
+        }
+
+        self.getFullPathBig = function (fileId) {
+          fileId = self.getFileId(fileId);
+          return self.fullPath + self.data[fileId].fileId + "-big.jpg";
+        }
+
+        self.setIndPath = function (path, filename) {
           self.ind.length = self.data.length;
           self.ind.end = self.data.length;
           if (self.ind.end > self.ind.thumbLength) self.ind.end = self.ind.thumbLength;
+          self.fullPath = path + filename;
         }
+
+        self.setImage = function setImage(fileId) {
+          self.ind.bigInd = 1*fileId;
+        };
 
         self.db = GetData.get({filename: $routeParams.pageId}, function(images) {
           self.data = images.data;
-          self.setInd();
-
+          self.setIndPath(images.path, images.filename);
         	self.thumbs = images.data.slice(self.ind.start, self.ind.end);
-
-        	self.path = images.path + images.filename;
-          self.setImage(self.getFirstImg);
         });
-
-        self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
-        };
-
-        self.nextImage = function () {
-          self.ind.start++;
-          self.ind.end++;
-
-          self.temp = self.thumbs.shift();
-          self.files.push(self.temp);
-          return self.temp;
-        }
-
-        self.prevImage = function () {
-          self.temp = self.files.pop();
-          self.files.unshift(self.temp);
-        }
 
         self.closePopUp = function(){
           self.showPopUpMsg = false;
@@ -58,19 +94,12 @@ angular.
         self.showPopUpMsg = false;
 
         self.openPopUp = function( fileId ) {
-          var x = fileId * 1;
-          /*while(x > 0) {
-            self.nextImage();
-            console.log(fileId + " " + x);
-            x--;
-          }*/
           self.setImage(fileId);
           self.showPopUpMsg = true;
         }
 
         self.nextBigImage = function () {
-          self.nextImage();
-          self.setImage(self.files[0].fileId);
+          self.ind.nextImage();
         }
 
       }
