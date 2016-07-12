b893f66a38a61e44a740f6ef3e39f39f4347dd42
sdmaxim <maxim.sidich@gmail.com>
2016-07-12 22:47:58 +0300

    12/07_22-47

--- slideshow/slideshow-c.js
+++ slideshow/slideshow-c.js
@@ -6,82 +6,74 @@ angular.
     templateUrl: 'slideshow/slideshow.html',
     controller: ['$routeParams', 'GetData', function ($routeParams, GetData) {
         var self = this;
-        self.fullPath = "";
-        self.ind = {
-          thumbLength : 7,
-          length : 0,
-          start : 0,
-          end : 0,
-          bigInd : 0,
-          nextImage : function () {
-            this.bigInd++;
-            this.start++;
-            this.end++;
-            if (this.start > this.length-1) {
-              this.start = 0;
-            }
-            if (this.end > this.length-1) {
-              this.end = 0;
-            }
-            if (this.bigInd > this.length-1) {
-              this.bigInd = 0;
-            }
+        self.data = [];
+        self.thumbs = [];
+        var fullPath = "";
+        var thumbLength = 10;
+        var length = 0;
+        var start = 0;
+        var end = 0;
+        self.bigInd = 0;
 
+        GetData.get({filename: $routeParams.pageId}, function(images) {
+          self.data = images.data;
+          length = self.data.length;
+          end = self.data.length-1;
+          if (end > thumbLength-1) end = thumbLength-1;
+          fullPath = images.path + images.filename;
+          self.thumbs = images.data.slice(start, end);
+        });
+
+        self.nextImage = function () {
+            start++;
+            end++;
+
+            validInd();
             self.thumbs.shift();
-            self.thumbs.push(self.data[this.end]);
+            self.thumbs.push(self.data[end]);
+        };
 
-            console.log(this.start + " " + this.end);
-          },
-          prevImage : function () {
-            this.start--;
-            this.end--;
-            this.bigInd--;
-            if (this.start < 0) {
-              this.start = this.length-1;
-            }
-            if (this.end < 0) {
-              this.end = this.length-1;
-            }
-            if (this.bigInd < 0) {
-              this.bigInd = this.length-1;
-            }
+        self.prevImage = function () {
+            start--;
+            end--;
 
+            validInd();
             self.thumbs.pop();
-            self.thumbs.unshift(self.data[this.start]);
-            console.log(this.start + " " + this.end);
-          }
+            self.thumbs.unshift(self.data[start]);
+        };
+
+        var validInd = function () {
+            if (start > length-1) {
+              start = 0;
+            }
+            if (end > length-1) {
+              end = 0;
+            }
+            if (start < 0) {
+              start = length-1;
+            }
+            if (end < 0) {
+              end = length-1;
+            }
         }
 
-        self.getFileId = function (fileId) {
+        var getFileId = function (fileId) {
           if (!fileId) fileId = 0;
-          fileId += self.ind.start;
-          if (fileId > self.ind.length-1)
-          fileId -= self.ind.length - 1;
+          fileId += start;
+          if (fileId > length-1)
+          fileId -= length - 1;
           return fileId;
         }
 
-        self.getFullPathSmall = function (fileId) {
-          fileId = self.getFileId(fileId);
-          return self.fullPath + self.data[fileId].fileId + "-small.jpg";
+        self.getFullImgPath = function (fileId, type) {
+          if (!fullPath) return "";
+          switch (type) {
+            case "big" : type = "-big.jpg"; break;
+            default : type = "-small.jpg"; break;
+          }
+          fileId = getFileId(fileId);
+          return fullPath + self.data[fileId].fileId + type;
         }
-
-        self.getFullPathBig = function (fileId) {
-          fileId = self.getFileId(fileId);
-          return self.fullPath + self.data[fileId].fileId + "-big.jpg";
-        }
-
-        self.setIndPath = function (path, filename) {
-          self.ind.length = self.data.length;
-          self.ind.end = self.data.length-1;
-          if (self.ind.end > self.ind.thumbLength-1) self.ind.end = self.ind.thumbLength-1;
-          self.fullPath = path + filename;
-        }
-
-        self.db = GetData.get({filename: $routeParams.pageId}, function(images) {
-          self.data = images.data;
-          self.setIndPath(images.path, images.filename);
-        	self.thumbs = images.data.slice(self.ind.start, self.ind.end);
-        });
 
         self.closePopUp = function(){
           self.showPopUpMsg = false;
@@ -90,12 +82,8 @@ angular.
         self.showPopUpMsg = false;
 
         self.openPopUp = function( fileId ) {
-          self.ind.bigInd = fileId;
+          self.bigInd = fileId;
           self.showPopUpMsg = true;
-        }
-
-        self.nextBigImage = function () {
-          self.ind.nextImage();
         }
 
       }
