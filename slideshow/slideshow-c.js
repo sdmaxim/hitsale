9aed112bbe81676122838b040c5f646694259ef5
sdmaxim <maxim.sidich@gmail.com>
2016-07-19 23:50:53 +0300

    19/07_23-50

--- slideshow/slideshow-c.js
+++ slideshow/slideshow-c.js
@@ -6,127 +6,132 @@ angular.
     templateUrl: 'slideshow/slideshow.html',
     controller: ['$routeParams', 'GetData', function ($routeParams, GetData) {
         var self = this;
-        self.data = [];
-        var fullPath = "";
+        self.data = []; //Список картинок и свойств
+        var fullPath = ""; //Полный путь к файлу без ID
         var startFilmWidth = 1000; //начальная длина для начального рендеринга
         self.filmWidth = startFilmWidth; //Длина пленки
-        self.length = 0; //Количество кадров в пленке
+        length = 0; //Количество кадров в пленке
         var start = 0; //Первый видимый кадр
         var end = 0; //последний видимый кадр
         var bigInd = 0; //Индек больших картинок
-        var widthShowenFrames = 0; //Длина видимых кадров
-        var framesCount = 0; //Счетчик кадров
-        var slideShowWindow = 640;
-        var framesBetweenWidth = 2;
-        var cursor = 0;
-        var delta = 0;
-        self.shift = 0;
-        var last = 0;
-        self.leftBigMargine = 0;
+        var showenFramesL = 0; //Длина видимых кадров
+        var counter = 0; //Счетчик кадров
+        var showWindowW = 640; //Ширина окна показа ленты
+        var space = 2; //Расстояние между кадрами
+        var cursor = 0; //Направление движения
+        var delta = 0; //Текущая величина сдвига ленты
+        self.shift = 0; //Общий сдвиг ленты
+        self.bigMargine = 150; //Отступ от левого края для больших картинок
 
         GetData.get({filename: $routeParams.pageId}, function(images) {
           self.data = images.data;
-          self.length = self.data.length;
-          end = self.length-1;
+          length = self.data.length;
+          end = length-1;
           fullPath = images.path + images.filename;
 
           //Добавляем ширины кадров к их обектам, вычисляем длину пленки и последний видимый кадр
-          self.data.forEach(function(item, framesCount, arr) {
+          self.data.forEach(function(item, counter, arr) {
             var img = new Image();
             img.onload = function() {
-              self.data[framesCount].width = this.width;
-              if (self.filmWidth - startFilmWidth <= slideShowWindow) {
-                end = framesCount;
+              //Добавляем свойство ширина к каждому кадру
+              self.data[counter].width = this.width;
+              if (self.filmWidth - startFilmWidth <= showWindowW) {
+                end = counter;
               }
-              self.filmWidth += this.width + framesBetweenWidth;
+              self.filmWidth += this.width + space;
 
-              if (framesCount == self.length-1) {
+              if (counter == length-1) {
                 self.filmWidth -= startFilmWidth;
-                if (end == 0) end = self.length-1;
+                if (end == 0) end = length-1;
               }
             }
-            img.src = self.getSmallImg(framesCount);
+            img.src = self.getSmallImg(counter);
             var imgBig = new Image();
             imgBig.onload = function() {
-              self.data[framesCount].widthBig = this.width;
+              self.data[counter].widthBig = this.width;
             }
-            imgBig.src = self.getFullImgPath(framesCount, "big");
+            imgBig.src = self.getFullImgPath(counter, "big");
           });
         });
 
-        var nextImageValid = function () {
-          if ((end == self.length-1) && (cursor == 0)) 
-            return true
-          else
-            return false;
-        }
-
-        var getLeftBigMargine = function () {
-          self.leftBigMargine = (window.innerWidth-self.data[bigInd].widthBig) / 2;
+        var getbigMargine = function () {
+          self.bigMargine = (window.innerWidth-self.data[bigInd].widthBig) / 2;
         }
         
         self.nextImage = function () {
-          //Выйти если превышена правая граница пленки+
-          if (nextImageValid()) return;
+          bigInd++;
+          if (bigInd > length-1) bigInd = 0;
+          getbigMargine();
 
-          //Подсчет длины видимых кадров
-          widthShowenFrames = 0;
-          var framesCount = end+1;
-          while (((widthShowenFrames < slideShowWindow) || (cursor < 0)) && (framesCount > 0)) {
-            framesCount--;
-            widthShowenFrames += self.data[framesCount].width + framesBetweenWidth;
-          }
+          //Выйти если превышена правая граница пленки
+          if ((end == length-1) && (cursor == 0)) return;
 
-          if ((cursor < 0) || ((cursor == 0) && (start == 0))) {
-            delta = widthShowenFrames - slideShowWindow - framesBetweenWidth;
+          if (showenFramesL == showWindowW) cursor = 1;
+          if (cursor <= 0) {
+            showenFramesL = 0;
+            counter = start-1;
+            cursor = 1;
+            while ((showenFramesL < showWindowW) && (counter < length-1)) {
+              counter++;
+              showenFramesL += self.data[counter].width + space;
+            }
+            end = counter;
+            delta = showenFramesL - showWindowW;
           } else {
-            delta = self.data[end].width + framesBetweenWidth;
+            end++;
+            if (end > length-1) {
+              end = length-1;
+              cursor = 0;
+              return;
+            }
+            delta = self.data[end].width + space;
+            showenFramesL += delta;
+
+            start--;
+            while ((showenFramesL >= showWindowW) && (start < length-1)) {
+              start++;
+              showenFramesL -= (self.data[start].width + space);
+            }
+            showenFramesL += self.data[start].width + space;
           }
           self.shift -= delta;
-          cursor = 1;
-
-          console.log(start + " " + end + " " + " delta " + delta);
-
-          bigInd++;
-          start = framesCount;
-          end++;
-          if (end == self.length) {
-            end--;
-            cursor = 0;
-          }
-          getLeftBigMargine();
         };
 
         self.prevImage = function () {
-          //Выйти если превышена правая граница пленки
+          bigInd--;
+          if (bigInd < 0) bigInd = length-1;
+          getbigMargine();
+          //Выйти если превышена левая граница пленки
           if ((start == 0) && (cursor == 0)) return;
 
-          //Подсчет длины видимых кадров
-          widthShowenFrames = 0;
-          var framesCount = start-1;
-          while (((widthShowenFrames < slideShowWindow) || (cursor > 0) || (nextImageValid())) && (framesCount < self.length-1)) {
-            framesCount++;
-            widthShowenFrames += self.data[framesCount].width + framesBetweenWidth;
-          }
-
+          if (showenFramesL == showWindowW) cursor = -1;
           if (cursor >= 0) {
-            delta = widthShowenFrames - slideShowWindow - framesBetweenWidth;
+            showenFramesL = 0;
+            counter = end+1;
+            cursor = -1;
+            while ((showenFramesL < showWindowW) && (counter > 0)) {
+              counter--;
+              showenFramesL += self.data[counter].width + space;
+            }
+            delta = showenFramesL - showWindowW;
           } else {
-            delta = self.data[start].width + framesBetweenWidth;
+            start--;
+            if (start < 0) {
+              start = 0;
+              cursor = 0;
+              return;
+            }
+            delta = self.data[start].width + space;
+            showenFramesL += delta;
+
+            end++;
+            while ((showenFramesL >= showWindowW) && (end > 0)) {
+              end--;
+              showenFramesL -= (self.data[end].width + space);
+            }
+            showenFramesL += self.data[end].width + space;
           }
           self.shift += delta;
-          cursor = -1;
-
-          console.log(start + " " + end + " " + " delta " + delta);
-
-          bigInd--;
-          start--;
-          end = framesCount;
-          if (start == -1) {
-            start = 0;
-            cursor = 0;
-          }
-          getLeftBigMargine();
         };
 
         self.getShowFlag = function (imgInd) {
